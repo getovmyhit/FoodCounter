85cd32a4080812d9937f5b996f10c5e641b479a1
Anna <annvolina@bk.ru>
2023-03-07 13:55:46 +0300

    btn

---
+++ app/src/main/java/com/example/foodcounter/RegActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg)
+
+    }
+}
